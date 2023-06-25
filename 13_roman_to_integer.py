0d2f57149c9b24582dfe8e5a9e4c6e0ae9544a78
wendellavila <wendelljcavila@gmail.com>
2023-06-25 20:35:12 -0300

    13. Roman to Integer

---
+++ 13_roman_to_integer.py
@@ -0,0 +1,42 @@
+import unittest
+
+def romanToInt(s: str) -> int:
+    toInt = 0
+    values = {'I': 1,'V': 5,'X': 10,'L': 50,'C': 100,'D': 500,'M': 1000}
+    skip_next = False
+
+    for i in range(0, len(s)):
+        if skip_next == False:
+            if i+1 < len(s):
+                if values[s[i]] >= values[s[i+1]]:
+                    toInt += values[s[i]]
+                    skip_next = False
+                else:
+                    toInt += values[s[i+1]] - values[s[i]]
+                    skip_next = True
+            else:
+                toInt += values[s[i]]
+                skip_next = False
+        else:
+            skip_next = False
+    return toInt
+
+class TestSolution(unittest.TestCase):
+    def test_case_1(self):
+        INPUT = "III"
+        EXPECTED = 3
+        OUTPUT = romanToInt(INPUT)
+        self.assertEqual(OUTPUT, EXPECTED)
+    def test_case_2(self):
+        INPUT = "LVIII"
+        EXPECTED = 58
+        OUTPUT = romanToInt(INPUT)
+        self.assertEqual(OUTPUT, EXPECTED)
+    def test_case_3(self):
+        INPUT = "MCMXCIV"
+        EXPECTED = 1994
+        OUTPUT = romanToInt(INPUT)
+        self.assertEqual(OUTPUT, EXPECTED)
+
+if __name__ == '__main__':
+    unittest.main()
