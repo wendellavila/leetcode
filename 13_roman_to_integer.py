12d6973f57b5899f565da0a7f01848f1c1d088a5
wendellavila <wendelljcavila@gmail.com>
2023-06-26 17:39:52 -0300

    Added docstrings

--- 13_roman_to_integer.py
+++ 13_roman_to_integer.py
@@ -1,6 +1,21 @@
 import unittest
+"""
+LeetCode problem
+13. Roman to Integer
+
+Given a roman numeral, convert it to an integer.
+
+https://leetcode.com/problems/roman-to-integer/
+"""
 
 def romanToInt(s: str) -> int:
+    """
+    For each character in the string:
+    If current character have a bigger corresponding value than the next character, add it to the total and repeat for the next.
+    If current character have a smaller corresponding value than the next character, add the difference between the next character and the
+    current to the total, skip the next character, and repeat for the character after the next.
+
+    """
     toInt = 0
     values = {'I': 1,'V': 5,'X': 10,'L': 50,'C': 100,'D': 500,'M': 1000}
     skip_next = False
@@ -22,6 +37,7 @@ def romanToInt(s: str) -> int:
     return toInt
 
 class TestSolution(unittest.TestCase):
+    """Unit tests using unittest module"""
     def test_case_1(self):
         INPUT = "III"
         EXPECTED = 3
--- 234_palindrome_linked_list.py
+++ 234_palindrome_linked_list.py
@@ -1,16 +1,27 @@
 import unittest
 from typing import Optional
+"""
+LeetCode problem
+234. Palindrome Linked List
 
-# Definition for singly-linked list.
+Given the head of a singly linked list, return true if it is a 
+palindrome or false otherwise.
+
+https://leetcode.com/problems/palindrome-linked-list/
+"""
 class ListNode:
+    """Definition for singly-linked list"""
     def __init__(self, val=None, next=None):
         self.val = val
         self.next = next
 
 def isPalindrome(head: Optional[ListNode]) -> bool:
+    """For each value in the list, concatenate it to the start of a string and to the end of another string
+    if the final strings are equal, input is palindrome.
+    """
     values = ""
     values_inverse = ""
-    
+
     while head is not None:
         values = values + str(head.val)
         values_inverse = str(head.val) + values_inverse
@@ -22,6 +33,7 @@ def isPalindrome(head: Optional[ListNode]) -> bool:
         return False
 
 class TestSolution(unittest.TestCase):
+    """Unit tests using unittest module"""
     def build_linked_list(self, values) -> ListNode:
         if len(values) > 0:
             head = ListNode(val=values[0])
